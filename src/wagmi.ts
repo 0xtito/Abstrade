0b46dd6cc03c4b5b13032eeba93c232e4f8f773b
0xtito <welovethepenguins@gmail.com>
2023-03-09 21:49:30 -0500

    init

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,25 +1,28 @@
-import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { configureChains, createClient } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient } from "wagmi";
+import { goerli, mainnet, gnosisChiado, gnosis } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
   [
-    publicProvider(),
+    mainnet,
+    ...(process.env.NODE_ENV === "development"
+      ? [goerli, gnosisChiado]
+      : [gnosis]),
   ],
-)
+  [publicProvider()]
+);
 
 const { connectors } = getDefaultWallets({
-  appName: 'My wagmi + RainbowKit App',
+  appName: "Scaling Ethereum 2023",
   chains,
-})
+});
 
 export const client = createClient({
   autoConnect: true,
   connectors,
   provider,
   webSocketProvider,
-})
+});
 
-export { chains }
+export { chains };
