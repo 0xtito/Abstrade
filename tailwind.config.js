c9b11831146de349bd0d8d561d6a659f95ca5920
0xtito <welovethepenguins@gmail.com>
2023-03-13 01:21:16 -0400

    config/added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    "tailwindcss",
+    "autoprefixer",
+    "@tailwindcss/forms",
+    "@tailwindcss/aspect-ratio",
+  ],
+};
