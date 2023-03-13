6e944b174c185b3a333a84c658ce857a30712a75
0xtito <welovethepenguins@gmail.com>
2023-03-13 01:20:18 -0400

    Dashboard components

---
+++ src/components/TradingViewWidget.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useRef } from "react";
+import { EffectCallback } from "react";
+import { createChart, ColorType } from "lightweight-charts";
+
+let tvScriptLoadingPromise = null;
+
+// type EffectCallback = () => void | Destructor;
+
+interface ChartProps {
+  data: any;
+  colors?: {
+    backgroundColor?: string;
+    lineColor?: string;
+    textColor?: string;
+    areaTopColor?: string;
+    areaBottomColor?: string;
+  };
+}
+
+export function TradingViewWidget(props: ChartProps) {
+  const {
+    data,
+    colors: {
+      backgroundColor = "white",
+      lineColor = "#2962FF",
+      textColor = "black",
+      areaTopColor = "#2962FF",
+      areaBottomColor = "rgba(41, 98, 255, 0.28)",
+    } = {},
+  } = props;
+
+  const chartContainer = document.createElement("div");
+  chartContainer.id = "chart";
+  const chartContainerRef = useRef<HTMLDivElement>(chartContainer);
+  // const jsxChart = <div></div>
+  // const mainChart = document.createElement("div");
+
+  useEffect(() => {
+    const handleResize = () => {
+      chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+    };
+
+    const chart = createChart(chartContainerRef.current, {
+      layout: {
+        background: { type: ColorType.Solid, color: backgroundColor },
+        textColor,
+      },
+      height: 300,
+    });
+    chart.timeScale().fitContent();
+
+    const newSeries = chart.addAreaSeries({
+      lineColor,
+      topColor: areaTopColor,
+      bottomColor: areaBottomColor,
+    });
+    newSeries.setData(data);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+
+      chart.remove();
+    };
+  }, [
+    data,
+    backgroundColor,
+    lineColor,
+    textColor,
+    areaTopColor,
+    areaBottomColor,
+  ]);
+
+  return <div className="relative " ref={chartContainerRef} />;
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
-export { Account } from './Account'
+export { Account } from "./Account";
+export { WalletDropDown } from "./WalletDropDown";
+export { TradingViewWidget } from "./TradingViewWidget";
+export { Divider } from "./Divider";
