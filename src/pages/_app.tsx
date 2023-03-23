4f608d68c930eea005634bb2fa1fccb2986c416a
0xtito <welovethepenguins@gmail.com>
2023-03-23 14:42:32 -0400

    almost finalized except for adding more components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,127 +1,21 @@
 import "../styles/global.css";
 import type { AppProps } from "next/app";
 import NextHead from "next/head";
-import * as React from "react";
-import {
-  WagmiConfig,
-  Client as WagmiClient,
-  createClient,
-  Connector,
-  Client,
-} from "wagmi";
-import { CHAIN_NAMESPACES } from "@web3auth/base";
-import { useAccount } from "wagmi";
+import React, { useEffect } from "react";
+import { WagmiConfig } from "wagmi";
 
-import { chains, provider } from "../wagmi";
-import { Web3Auth } from "@web3auth/modal";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-// import Web3AuthAAConnectorInstance from "../connectors/Web3AuthAAConnectorInstance";
+import { client } from "../wagmi";
 
-// import { Web3AuthAAConnector } from "../connectors/Web3AuthAAConnector";
-import { Web3AuthAAConnector } from "../connectors/TWeb3AuthAAConnector";
-
-function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   const [mounted, setMounted] = React.useState(false);
-  const [client, setClient] = React.useState<any | null>(null);
-  // const { connector } = useAccount();
-  // const { isConnected, connector } = useAccount();
-  const [web3AuthInstance, setWeb3AuthInstance] =
-    React.useState<Web3Auth | null>(null);
 
-  // const client = createClient({
-  //   autoConnect: true,
-  //   connectors: [
-  //     new MetaMaskConnector({ chains }),
-  //     Web3AuthAAConnectorInstance(chains),
-  //   ],
-  //   provider,
-  // });
-
-  React.useEffect(() => {
-    const init = async () => {
-      const { Web3Auth } = await import("@web3auth/modal");
-      const { OpenloginAdapter } = await import("@web3auth/openlogin-adapter");
-      const { MetaMaskConnector } = await import("wagmi/connectors/metaMask");
-
-      console.log("inside init");
-      try {
-        const web3AuthInstance = new Web3Auth({
-          clientId: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID!,
-          chainConfig: {
-            chainNamespace: CHAIN_NAMESPACES.EIP155,
-            chainId: chains[0].id.toString(),
-            rpcTarget: chains[0].rpcUrls.default.http[0],
-          },
-          web3AuthNetwork: "testnet",
-        });
-
-        const openloginAdapter = new OpenloginAdapter({
-          adapterSettings: {
-            network: "testnet",
-            uxMode: "popup",
-            whiteLabel: {
-              name: "Scaling Ethereum 2023",
-              logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
-              logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
-              defaultLanguage: "en",
-              dark: true, // whether to enable dark mode. defaultValue: false
-            },
-          },
-        });
-
-        web3AuthInstance.configureAdapter(openloginAdapter);
-
-        await web3AuthInstance.initModal();
-
-        setWeb3AuthInstance(web3AuthInstance);
-
-        const connectors = [
-          new Web3AuthAAConnector({
-            chains,
-            options: {
-              web3AuthInstance,
-              // will either remove or configure projectId
-              projectId: "1",
-              entryPointAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-              accountFactoryAddress:
-                "0x6C583EE7f3a80cB53dDc4789B0Af1aaFf90e55F3",
-              // "0x09c58cf6be8E25560d479bd52B4417d15bCA2845",
-            },
-          }),
-          new MetaMaskConnector({ chains }),
-        ];
-
-        const _client = createClient({
-          autoConnect: false,
-          connectors,
-          provider,
-        });
-        setClient(_client);
-        setMounted(true);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    if (!web3AuthInstance) {
-      console.log("not connected");
-      init();
-    } else if (web3AuthInstance) {
-      console.log("already connected");
-      // await web3AuthInstance.init();
-    }
+  useEffect(() => {
+    setMounted(true);
   }, []);
 
-  if (!client && !mounted) {
+  if (!mounted) {
     return null;
   }
-
-  // if (typeof window !== "undefined") {
-  //   window.addEventListener("beforeunload", async () => {
-  //     console.log(web3AuthInstance, "unloading");
-  //     await web3AuthInstance?.logout();
-  //   });
-  // }
-
   return (
     <WagmiConfig client={client}>
       <NextHead>
@@ -132,5 +26,3 @@ function App({ Component, pageProps }: AppProps) {
     </WagmiConfig>
   );
 }
-
-export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,72 @@
-import React, { Fragment, useEffect } from "react";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React, { useEffect } from "react";
 import { useAccount, useConnect } from "wagmi";
 import { SignIn } from "../components/SignIn";
-
-import { Account } from "../components";
+import AbstradeLogo from "../static/images/abstrade-v2-light.png";
+import Image from "next/image";
+import { Spinner } from "../components/Spinner";
+import { useRouter } from "next/router";
 
 function Page() {
-  const { isConnected, connector } = useAccount();
+  const { isConnected, isConnecting, connector } = useAccount();
   const { connect, connectors } = useConnect();
-
-  console.log("isConnected", isConnected);
+  const router = useRouter();
 
   useEffect(() => {
-    console.log(connector);
-  });
+    if (isConnected) {
+      router.push("/app");
+    }
+  }, [isConnected]);
 
   return (
-    <Fragment>
-      <SignIn />
-      {<Account />}
+    <div className="max-h-screen">
+      <div className="flex min-h-full flex-col justify-center pt-12 sm:px-6 lg:px-8 ">
+        <Image
+          className="mx-auto h-auto w-auto"
+          src={AbstradeLogo}
+          alt="Your Company"
+        />
+      </div>
+      {isConnecting || isConnected ? (
+        <div className="flex flex-col items-center pt-10">
+          <Spinner />
+          <p className="mt-10 text-lg">Signing you in now...</p>
+        </div>
+      ) : (
+        <SignIn />
+      )}
+
+      {/* <SignIn />
+      <Spinner withFramerMotion={false} /> */}
+      <div className="absolute inset-x-0 top-[calc(50%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(50%-17rem)]">
+        <svg
+          className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+        >
+          <path
+            fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+            fillOpacity=".3"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9089FC" />
+              <stop offset={1} stopColor="#FF80B5" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+      {/* {<Account />}
       <button onClick={() => connector?.disconnect()}>
         {isConnected ? "disconnect" : ""}
-      </button>
-    </Fragment>
+      </button> */}
+    </div>
   );
   // return (
   //   <>
