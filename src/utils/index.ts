e7b27bc53bba7b97bd5266bfba8db40cc7e2231d
0xtito <welovethepenguins@gmail.com>
2023-03-24 22:02:03 -0400

    updated utils folder

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ export * from "./findWalletIcon";
 export * from "./classNames";
 export * from "./handleSidebarToggle"; // export { handleSideBarToggle } from "./handleSidebarToggle";
 export * from "./BaseAccountAPI"; // export { BaseAccountAPI } from "./BaseAccountAPI";
-export * from "./SimpleAccountAPI";
+export * from "./OldSimpleAccountAPI";
 export * from "./getAAProvider";
 export * from "./getAASigner";
 export * from "./getOpenloginAdapterConfig";
